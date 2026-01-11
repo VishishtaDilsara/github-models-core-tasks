32e5210954a3afbeae6993c33df9060eecfcc742
VishishtaDilsara <rgvdilsara2002@gmail.com>
2026-01-11 14:07:50 +0530

    task03-finalized

--- task/task03-embedding-mistral.js
+++ task/task03-embedding-mistral.js
@@ -8,9 +8,8 @@ async function generateEmbedding() {
         method: "POST",
         headers: {
           Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-          Accept: "application/vnd.github+json",
-          "X-GitHub-Api-Version": "2022-11-28",
           "Content-Type": "application/json",
+          Accept: "application/json",
         },
         body: JSON.stringify({
           model: "openai/text-embedding-3-small",
